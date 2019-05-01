355b0c65bc2c6c7afd11a2ee21cba6d2ed3ef032
mxm001 <17750101+mxm001@users.noreply.github.com>
2019-05-01 19:42:39 -0300

    version final modificable y listo para import

--- scrapAlMundoFromApi.py
+++ scrapAlMundoFromApi.py
@@ -2,11 +2,8 @@ import requests
 import json
 import datetime
 import time
+import argparse
 import jsonpickle
-# {"precioNeto":"25190.20","origen":{"iata":"BUE","nombre":"Buenos Aires - Argentina"},
-# "destino":{"iata":"MIA","nombre":"Miami - Estados Unidos"},
-# "dateD":"17-05-2019","dateR":"27-05-2019","cantDias":"10","aerolinea":"AM","airname":"Aerom\u00e9xico","precio":"25190.20"}
-# https://almundo.com.ar/flights/offers/api/offers?months=5&from=BUE&to=MIA&page=2&size=100
 
 
 class Lugares (object):
@@ -29,46 +26,32 @@ class Datos (object):
         self.airname = airname
         self.precio = precio
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-m","--Mes",  type=int, help="Número del mes, por defecto toma mes actual + 1",default=0)
+parser.add_argument("-if","--IataFrom",  type=str, help="Iata Desde, default BUE",default="BUE")
+parser.add_argument("-it","--IataTo",  type=str, help="Iata hasta, default MIA",default="MIA")
+parser.add_argument("-n","--Numero",  type=int, help="Numero de página, default 1",default=1)
+args = parser.parse_args()
+def executeScrap( ):
+    lista = list()
+    numeroMes = str(datetime.datetime.now().month+1)
+    if args.Mes<13 and args.Mes>0:
+        numeroMes=args.Mes
 
-lista = list()
-
-numeroMes = str(datetime.datetime.now().month+1)
-iataFrom = "BUE"
-iataTo = "MIA"
-pageNum = 1
-pageSize = 100
-url = "https://almundo.com.ar/flights/offers/api/offers?months="+numeroMes + \
-    "&from="+iataFrom+"&to="+iataTo+"&page=" + \
-    str(pageNum)+"&size="+str(pageSize)
-firstReq = requests.get(url)
-print(firstReq)
-y = firstReq.json()
-for offer in y["offers"]:
-    ingreso = Datos()
-    ingreso.origen = Lugares(str(offer["from"]),str(offer["from_city_name"]+" - "+offer["from_country_name"]))
-    ingreso.destino = Lugares(str(offer["to"]),str(offer["to_city_name"]+" - "+offer["to_country_name"]))
-    ingreso.precioNeto = offer["total_price"]
-    ingreso.dateD = offer["departure"]
-    ingreso.dateR = offer["returning"]
-    ingreso.cantDias = offer["stay_duration"]
-    ingreso.aerolinea = offer["airline"]
-    ingreso.airname = offer["airline_name"]
-    ingreso.precio = offer["total_price"]
-    lista.append(ingreso)
-total = y["pagination"]["total"]
-totalRestante = total-int(pageSize)
-maxPage = total/int(pageSize)
-pageNum = pageNum+1
-
-while pageNum <= maxPage:
-    requrl = "https://almundo.com.ar/flights/offers/api/offers?months="+numeroMes + "&from="+iataFrom+"&to="+iataTo+"&page="+str(pageNum)+"&size="+str(pageSize)
-    print(requrl)
-    req = requests.get(requrl)
-    for offer in req.json()["offers"]:
+    iataFrom = args.IataFrom
+    iataTo = args.IataTo
+    pageNum = args.Numero
+    pageSize = 100
+    url = "https://almundo.com.ar/flights/offers/api/offers?months="+numeroMes + \
+        "&from="+iataFrom+"&to="+iataTo+"&page=" + \
+        str(pageNum)+"&size="+str(pageSize)
+    firstReq = requests.get(url)
+    print(firstReq)
+    y = firstReq.json()
+    for offer in y["offers"]:
         ingreso = Datos()
-        ingreso.origen = Lugares(offer["from"],offer["from_city_name"]+" - "+offer["from_country_name"])
-        ingreso.destino = Lugares(offer["to"],offer["to_city_name"]+" - "+offer["to_country_name"])
-
+        ingreso.origen = Lugares(str(offer["from"]),str(offer["from_city_name"]+" - "+offer["from_country_name"]))
+        ingreso.destino = Lugares(str(offer["to"]),str(offer["to_city_name"]+" - "+offer["to_country_name"]))
         ingreso.precioNeto = offer["total_price"]
         ingreso.dateD = offer["departure"]
         ingreso.dateR = offer["returning"]
@@ -77,9 +60,34 @@ while pageNum <= maxPage:
         ingreso.airname = offer["airline_name"]
         ingreso.precio = offer["total_price"]
         lista.append(ingreso)
+    total = y["pagination"]["total"]
+    totalRestante = total-int(pageSize)
+    maxPage = total/int(pageSize)
     pageNum = pageNum+1
 
-    time.sleep(3)
-json_string = jsonpickle.encode(lista, unpicklable=False)
-with open("Resultado mes"+numeroMes+" "+iataFrom+"-"+iataTo+".json", "w") as arch:
-    arch.write(json_string)
+    while pageNum <= maxPage:
+        requrl = "https://almundo.com.ar/flights/offers/api/offers?months="+numeroMes + "&from="+iataFrom+"&to="+iataTo+"&page="+str(pageNum)+"&size="+str(pageSize)
+        print(requrl)
+        req = requests.get(requrl)
+        for offer in req.json()["offers"]:
+            ingreso = Datos()
+            ingreso.origen = Lugares(offer["from"],offer["from_city_name"]+" - "+offer["from_country_name"])
+            ingreso.destino = Lugares(offer["to"],offer["to_city_name"]+" - "+offer["to_country_name"])
+
+            ingreso.precioNeto = offer["total_price"]
+            ingreso.dateD = offer["departure"]
+            ingreso.dateR = offer["returning"]
+            ingreso.cantDias = offer["stay_duration"]
+            ingreso.aerolinea = offer["airline"]
+            ingreso.airname = offer["airline_name"]
+            ingreso.precio = offer["total_price"]
+            lista.append(ingreso)
+        pageNum = pageNum+1
+
+        time.sleep(3)
+    json_string = jsonpickle.encode(lista, unpicklable=False)
+    with open("Resultado mes"+numeroMes+" "+iataFrom+"-"+iataTo+".json", "w") as arch:
+        arch.write(json_string)
+
+if __name__ == '__main__':
+	executeScrap()
